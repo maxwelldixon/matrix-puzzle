5a46342978e321b1e5ec979090df07fff8bc380b
mitortuga <26306720+maxwelldixon@users.noreply.github.com>
2019-09-02 18:50:29 -0400

    Updated to python 3

--- puzzlefinale.py
+++ puzzlefinale.py
@@ -1,7 +1,11 @@
 """ This program solves the First Day of School Challenge. It involves filling in 16 missing numbers (only use each number once) into 4 row equations and 4 column equations. First this algorithm finds the unique (no duplicate numbers) combinations for each row and places them into arrays named combosR1, combosR2, combosR3, and combosR4. Then it uses loops with decision structures to determine 4x4 matrices that use the numbers 1-16 only once out of the possible combinations for each row. Finally, the last four lines of the final loop structure use the unique matrices to test the equations in columns 0,1,2,3. If there is a match it prints the total number of iterations used to find the answer and the 4x4 matrix.
 """
+import time
+from datetime import timedelta
 
 def main():
+
+	start = time.time()
 
 	# list of possible numbers I can use 1-16
 	a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
@@ -17,7 +21,7 @@ def main():
 		for j in range(0,16):
 			for k in range(0,16):
 				for l in range(0,16):
-					# equation in the first row
+					# equation in row 1
 					if a[i] + b[j] + c[k] - d[l] == 31:
 						# eliminating duplicate numbers
 						if a[i] != b[j]:
@@ -28,7 +32,7 @@ def main():
 											if c[k] != d[l]:
 												# creates list of single arrays
 												combosR1.append([a[i], b[j], c[k], d[l]])
-												#print combosR1
+												
 
 	# empty array for Row 2 combos
 	combosR2 = []
@@ -39,7 +43,7 @@ def main():
 			for k in range(0,16):
 				for l in range(0,16):
 					if (a[i] * b[j]) % c[k] == 0:
-						# equation in the first row
+						# equation in row 2
 						if (a[i] * b[j]) / c[k] - d[l] == -3:
 						# eliminating duplicate numbers
 							if a[i] != b[j]:
@@ -50,7 +54,7 @@ def main():
 												if c[k] != d[l]:
 													# creates list of single arrays
 													combosR2.append([a[i], b[j], c[k], d[l]])
-													#print combosR2
+													
 
 	# empty array for Row 3 combos
 	combosR3 = []
@@ -60,7 +64,7 @@ def main():
 		for j in range(0,16):
 			for k in range(0,16):
 				for l in range(0,16):
-					# equation in the first row
+					# equation in row 3
 					if a[i] + b[j] + c[k] + d[l] == 42:
 					# eliminating duplicate numbers
 						if a[i] != b[j]:
@@ -71,7 +75,7 @@ def main():
 											if c[k] != d[l]:
 												# creates list of single arrays
 												combosR3.append([a[i], b[j], c[k], d[l]])
-												#print combosR3
+											
 
 	# empty array for Row 4 combos
 	combosR4 = []
@@ -82,7 +86,7 @@ def main():
 			for k in range(0,16):
 				for l in range(0,16):
 					if (a[i] + b[j]) % c[k] == 0: 
-						# equation in the first row
+						# equation in row 4
 						if (a[i] + b[j]) / c[k] + d[l] == 8:
 						# eliminating duplicate numbers
 							if a[i] != b[j]:
@@ -93,7 +97,7 @@ def main():
 												if c[k] != d[l]:
 													# creates list of single arrays
 													combosR4.append([a[i], b[j], c[k], d[l]])
-													#print combosR4
+													
 
 	# finds the unique matrices based on row combos calculated in the preceding loops
 	count = 0
@@ -137,22 +141,22 @@ def main():
 																				if combosR3[k][2] != combosR4[l][0] and combosR3[k][2] != combosR4[l][1] and combosR3[k][2] != combosR4[l][2] and combosR3[k][2] != combosR4[l][3]:
 																					# eliminates duplicates for R3[k][3] in columns C1, C2, C3, C4
 																					if combosR3[k][3] != combosR4[l][0] and combosR3[k][3] != combosR4[l][1] and combosR3[k][3] != combosR4[l][2] and combosR3[k][3] != combosR4[l][3]:
-																						#count += 1
-																						#print totalCount
-																						#print count
 																						# finds correct columns
 																						if combosR1[i][0] + combosR2[j][0] - combosR3[k][0] + combosR4[l][0] == 5:
 																							if combosR1[i][1] + combosR2[j][1] + combosR3[k][1] + combosR4[l][1] == 51:
 																								if combosR1[i][2] + combosR2[j][2] + combosR3[k][2] - combosR4[l][2] == 35:
 																									if (combosR1[i][3] * combosR2[j][3]) + combosR3[k][3] - combosR4[l][3] == 32:
 																										count += 1
-																										print count
-																										print totalCount
-																										print combosR1[i]
-																										print combosR2[j]
-																										print combosR3[k]
-																										print combosR4[l]
-																										print "              "
+																										elapsedTime = str(timedelta(seconds = time.time() - start))
+																										print(count + " solution was found in " + elapsedTime)
+																										print(totalCount + " combinations were tried.")
+																										print("The solution: ")
+																										print(combosR1[i])
+																										print(combosR2[j])
+																										print(combosR3[k])
+																										print(combosR4[l])
+																										
 
-main()				
+if __name__ == "__main__":
+	main()				
 
